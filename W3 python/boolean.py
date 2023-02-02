1bfacec6ae82b3167559b48c7816779b7e5e3501
Aleksandr Mezin <alexandr.mezin@gmail.com>
2023-02-02 10:40:21 +0600

    Some tasks were finished
    boolean
    operators

---
+++ W3 python/boolean.py
@@ -0,0 +1,16 @@
+#Boolean
+
+print(10 > 9)
+#will print "True"
+
+print(10 == 9)
+#will print "False"
+
+print(10 < 9)
+#will print "False"
+
+print(bool("abc"))
+#will print "True"
+
+print(bool(0))
+#will print "False"
