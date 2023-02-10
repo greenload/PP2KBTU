ca8f055f189f2712e2d5be7bb8eb985bf9b06231
Aleksandr Mezin <alexandr.mezin@gmail.com>
2023-02-10 18:26:03 +0600

    Task 2 from classes added

--- Lab works/Lab 3/Classes/classes2.py
+++ Lab works/Lab 3/Classes/classes2.py
@@ -4,15 +4,53 @@
 
 class Shape():
 
-    def __init__(self):
+    def __init__():
         pass
 
 
+class Square(Shape):
 
+    def __init__(self, length=0):
+        self.length = length
 
+    def area(self):
+        
+        self.length = int(input())
+        
+        areaValue = self.length * self.length
 
+        areaText = "Square area is:  {}"
 
+        print(areaText.format(areaValue))
 
 #Define a class named Rectangle which inherits from Shape class from task 2.
 #Class instance can be constructed by a length and width.
 #The Rectangle class has a method which can compute the area.
+
+class Rectangle(Shape):
+    
+    def __init__(self, length=0, width=0):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        self.length = int(input())
+        self.width = int(input())
+
+        areaValue = self.length * self.width
+
+        areaText = "Rectangle area is:  {}"
+
+        print(areaText.format(areaValue))
+
+square = Square()
+
+square.area()
+
+rectangle = Rectangle()
+
+rectangle.area()
+
+
+
+
--- Lab works/Lab 3/Classes/classes3.py
+++ Lab works/Lab 3/Classes/classes3.py
@@ -2,3 +2,22 @@
 #a method show to display the coordinates of the point
 #a method move to change these coordinates
 #a method dist that computes the distance between 2 points
+
+class Point():
+
+
+
+    def __init__(self):
+        pass
+
+    def show(self):
+        x = 2
+    
+    def move(self):
+        x = 3
+    
+    def dist(self):
+        x = 4
+
+    def drop(self):
+        x = 5
