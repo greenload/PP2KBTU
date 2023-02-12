c9ab6322965257fd4900f6f1d0762c23c9be23bd
Aleksandr Mezin <alexandr.mezin@gmail.com>
2023-02-12 23:28:32 +0600

    Functins tasks structure created

--- Lab works/Lab 3/Classes/classes5.py
+++ Lab works/Lab 3/Classes/classes5.py
@@ -1,2 +1,3 @@
 #Write a program which can filter prime numbers in a list by using filter function.
 #Note: Use lambda to define anonymous functions.
+
---
+++ Lab works/Lab 3/Functions/functions1_1.py
@@ -0,0 +1,5 @@
+import functions1
+
+ounces = float(input(f"Please, enter the weight in ounces:"))
+
+print(functions1.to_grams(ounces))
