bf6e46b89d3d38e61c02c68b66d564db0431f1fd
Aleksandr Mezin <alexandr.mezin@gmail.com>
2023-02-14 01:49:13 +0600

    Minor updates

--- Lab works/Lab 3/Functions/functions1_1.py
+++ Lab works/Lab 3/Functions/functions1_1.py
@@ -2,4 +2,6 @@ import functions1
 
 ounces = float(input(f"Please, enter the weight in ounces:"))
 
-print(functions1.to_grams(ounces))
+print(f"The weight in grams is: {functions1.to_grams(ounces)}")
+
+functions1.far_to_cels(int(input("Enter the Fahrenheit value: ")))
