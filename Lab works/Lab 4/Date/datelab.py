0f4b4cf0c0a97c9a26f88700f6922e8790928b4c
Alexandr Mezin <alexandr.mezin@gmail.com>
2023-02-16 13:56:44 +0600

    Lab 4 math tasks finished

--- Lab works/Lab 4/Date/datelab.py
+++ Lab works/Lab 4/Date/datelab.py
@@ -1,22 +1,22 @@
 import datetime
 
-class datelab:
-
-    def __init__(self):
-
-        pass
 #Write a Python program to subtract five days from current date.
-    def minusfive(self):
+def minus_five(self):
+    today = datetime.date.now()
 
-        pass
+        
+
+    pass
 #Write a Python program to print yesterday, today, tomorrow.
-    def yetoto(self):
-
-        pass
+def yetoto(self):
+    
+    pass
 #Write a Python program to drop microseconds from datetime.
-    def micdrop(self):
+def mic_drop(self):
 
-        pass
+    pass
 #Write a Python program to calculate two date difference in seconds.
-    def twodiff(self):
-        pass
+def two_diff(self, firstDate, secondDate):
+
+
+    pass
